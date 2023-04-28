36b64ff2c8c0e9020a5d0e619f267f6caf78d010
asinthelol <asinthexd@gmail.com>
2023-04-28 14:52:32 -0400

    Added files
    Exactly as title says.

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+// let mouseX;
+// let mouseY;
+// let demonImg = $('#demon');
+
+// // Parallax for background (mouse)
+// $(window).mousemove((e) => { 
+//   mouseX = e.clientX / 100;
+//   mouseY = e.clientY / 90;
+//   demonImg.css({
+//     'top': -(mouseY),
+//     'left': -(mouseX),
+//     'bottom': mouseY,
+//     'right': mouseX
+//   });
+// });
+
