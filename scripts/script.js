ce742b97ff54b68657567b2fb09b3335d547f725
asinthelol <asinthexd@gmail.com>
2023-05-03 14:53:36 -0400

    Changed load animation again
    Same as title.
    -Added versus and am trying to print them in console.
    - Added parallax on scroll. Gonna change the background.

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,42 @@
-// let mouseX;
-// let mouseY;
-// let demonImg = $('#demon');
+const bodyArea = $('body');
+const lyricContainer = $('#lyric-container');
+const songName = $('#song-name');
+const downArrow = $('#down-arrow');
+const anchorDownArr = $('#down-arrow-area > a');
 
-// // Parallax for background (mouse)
-// $(window).mousemove((e) => { 
-//   mouseX = e.clientX / 100;
-//   mouseY = e.clientY / 90;
-//   demonImg.css({
-//     'top': -(mouseY),
-//     'left': -(mouseX),
-//     'bottom': mouseY,
-//     'right': mouseX
-//   });
-// });
 
+// Allows scroll and makes down arrow clickable after fade-in
+downArrow.one('animationend', () => {
+  bodyArea.removeClass('no-scroll');
+  anchorDownArr.attr('href', '#lyric-container');
+})
+
+
+// Parllax on scroll for background
+let scrollValue;
+
+$(window).scroll( () => {
+  scrollValue = $(window).scrollTop();
+  lyricContainer.css({'background-position': `center ${scrollValue / 2}px`});
+});
+
+let verse, prechorus, chorus = [];
+let arrays = [verse, prechorus, chorus];
+
+console.log()
+async function getLyrics(arrays) {
+  const files = ['verse', 'pre-chorus', 'chorus'];
+  try {
+    for(file of files) {
+      let response = await fetch(`../json/${file}.json`);
+      if(response.ok) {
+        arrays[file] = await response.json();
+        console.log(arrays[file]);
+      }
+    }
+  } catch(err) {
+    console.log(err);
+  }
+}
+
+getLyrics(arrays);
